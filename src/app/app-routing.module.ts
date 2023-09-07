12e0edb28e3e45330d66691b72ec97781837881b
Vivek Saurabh <saurabhvivek30@gmail.com>
2023-09-08 00:13:12 +0530

    Added child1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Child1Component } from './child1/child1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component: Child1Component, path: 'child1'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
